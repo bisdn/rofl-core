77548b2bee91f239fdcaf503fbf8068550c18019
Marc Sune <marc.sune@eict.de>
2012-09-11 17:07:29 +0200

    cunixenv is now using a dynamic way of defining command line arguments, so that each and every daemon can define its own ones. Common parameters are, -h help, -D daemonize, -c confi-file and -d debug level

--- src/rofl/platform/unix/cunixenv.cc
+++ src/rofl/platform/unix/cunixenv.cc
@@ -4,9 +4,9 @@
 
 #include "cunixenv.h"
 
-
 extern char* optarg;
 
+#if 0
 /*static*/int cunixenv::debug = csyslog::EMERGENCY;
 /*static*/int cunixenv::n_buffers = 262144;
 /*static*/int cunixenv::block_size = 64;
@@ -16,12 +16,83 @@ extern char* optarg;
 /*static*/std::string cunixenv::controller_ip("127.0.0.1");
 /*static*/u_int16_t cunixenv::controller_port = 6633;
 /*static*/std::string cunixenv::config_filename("./default-cli.cfg");
-/*static*/int cunixenv::enable_hal = 0;
+/*static*/std::string cunixenv::hw_driver = "x86";
+
+/* Default options*/
+static struct option long_options[] = {
+			{ "daemonize", 0, 0, 'D' },
+			{ "help", 0, 0, 'h' },
+			{ "debug", 1, 0, 'd' },
+			{ "configfile", 1, 0, 'c' },
+		      /*{ "buffers", 1, 0, 'b' },
+			{ "blocksize", 1, 0, 's' },
+			{ "blocks", 1, 0, 'k' },
+			{ "framesize", 1, 0, 'r' },
+			{ "fwdengines", 1, 0, 'f' },
+			{ "ctl_ip_addr", 1, 0, 'i' },
+			{ "ctl_port", 1, 0, 'p' },
+			{ "hw_driver", 1, 0, 'H' },*/
+			{ NULL, 0, 0, 0 }
+};
+#endif
+
+/* Carg stuff */
+carg::carg(bool optional, 
+		int value_type,
+		char shortcut, 
+		std::string full_name, 
+		std::string description, 
+		std::string default_value):
+		optional(optional), 
+		value_type(value_type), 
+		shortcut(shortcut), 
+		full_name(full_name), 
+		description(description), 
+		default_value(default_value)
+{
+	current_value = default_value;
+	present = false;
+	
+	if(!(this->value_type == NO_ARGUMENT ||
+		this->value_type == REQUIRED_ARGUMENT|| 
+		this->value_type == OPTIONAL_ARGUMENT )) 
+			throw std::runtime_error("Unknown value type");
+}	
+
+std::string carg::parse_argument(char* optarg){
+	if(value_type != NO_ARGUMENT)
+		this->current_value = std::string(optarg);
+	present = true;
+	
+	return this->current_value;	
+}
+
+/*
+*
+* cunixenv
+*
+*/
+
+/* Constructor */ 
+cunixenv::cunixenv(std::vector<carg>* args){
+	
+	if(args)
+		arguments = *args;
+	else{
+		/*Push default arguments */
+		arguments.push_back(carg(true,REQUIRED_ARGUMENT,'d',"debug","debug level",std::string(""+(int)csyslog::EMERGENCY)));
+		arguments.push_back(carg(true,NO_ARGUMENT,'h',"help","Help message",""));
+		arguments.push_back(carg(false,REQUIRED_ARGUMENT,'c',"config-file","Config file","./default-cli.cfg"));
+		arguments.push_back(carg(true, NO_ARGUMENT,'D',"daemonize","Daemonize process",""));
+	}
+	parsed = false;
+}
 
 void
 cunixenv::usage(
 		char *argv0)
 {
+	#if 0
 	fprintf(stderr, "usage: %s [--daemonize] [--help]\n", argv0);
 	fprintf(stderr, "[--debug|-d] <int:debuglevel>\n");
 	fprintf(stderr, "[--blocksize|-s] <int:#pagenumbers>\n");
@@ -32,7 +103,40 @@ cunixenv::usage(
 	fprintf(stderr, "[--configfile|-c] <string:filename>\n");
 	fprintf(stderr, "[--ctl_ip_addr|-i] <ip:127.0.0.1>\n");
 	fprintf(stderr, "[--ctl_port|-p] <int:#portnum>\n");
-	fprintf(stderr, "[--hal|-a] <int: default(0) HAL(1)>\n");
+	fprintf(stderr, "[--hw_driver|-H] <string: {x86,bcm,octeon5650} default(x86) >\n");
+	exit(0);
+	#endif
+	using namespace std;
+	cerr << "usage: " << std::string(argv0)<<" {parameters}"<<endl; 
+	
+	string mandatory = "";
+	string optional = "";
+
+	for(std::vector<carg>::iterator it = this->arguments.begin(); it != this->arguments.end(); it++){
+		string tmp(""); 
+		if(it->optional)
+			tmp+="[";
+		tmp+="--"+it->full_name+"|-"+it->shortcut;
+		
+		if(it->optional)
+			tmp+="]";
+	
+		tmp +=" <"+it->description;
+		if(it->default_value != "")
+			tmp+=". default("+it->default_value+")";
+		tmp+=">\n";
+		
+		if(!it->optional)
+			mandatory += tmp; 
+		else
+			optional += tmp; 
+			
+	}
+	if(mandatory!= "")
+		cerr<<"Mandatory parameters:"<<endl<<mandatory;	
+	if(optional!= "")
+		cerr<<"Optional parameters:"<<endl<<optional;	
+	
 	exit(0);
 }
 
@@ -44,25 +148,55 @@ cunixenv::parse_args(
 {
 	int c;
 	int option_index;
+	std::string format;
+	struct option* long_options = (struct option*)calloc(1,sizeof(struct option)*(this->arguments.size()+1));
 
-	static struct option long_options[] = {
-			{ "daemonize", 0, 0, 'D' },
-			{ "help", 0, 0, 'h' },
-			{ "debug", 1, 0, 'd' },
-			{ "buffers", 1, 0, 'b' },
-			{ "blocksize", 1, 0, 's' },
-			{ "blocks", 1, 0, 'k' },
-			{ "framesize", 1, 0, 'r' },
-			{ "fwdengines", 1, 0, 'f' },
-			{ "configfile", 1, 0, 'c' },
-			{ "ctl_ip_addr", 1, 0, 'i' },
-			{ "ctl_port", 1, 0, 'p' },
-			{ "hal", 1, 0, 'a' },
-			{ NULL, 0, 0, 0 }
-	};
+	//Construct getopts stuff
+	for(std::vector<carg>::iterator it = this->arguments.begin(); it != this->arguments.end(); it++){
+		format += it->shortcut;
+		if(it->value_type == REQUIRED_ARGUMENT)
+			format+=":"; 
+		else if(it->value_type == OPTIONAL_ARGUMENT)
+			format+="::"; 
+
+		//Fill long_options row
+		struct option* tmp = &long_options[std::distance(this->arguments.begin(), it)];
+		tmp->has_arg = it->value_type;
+		tmp->name = it->full_name.c_str();
+		tmp->val = it->shortcut;
+	
+	}
+
+	//fprintf(stderr,"Using the following format-> %s \n",format.c_str());	
+	while(true){
+	
+		c = getopt_long(argc, argv, format.c_str(), long_options, &option_index);
+		if (c == -1)
+			break;
+		//TODO: might be better to have a map	
+		for(std::vector<carg>::iterator it = this->arguments.begin(); it != this->arguments.end(); it++){
+			if(it->shortcut == c){
+				it->parse_argument(optarg);
+				if(c == 'h')
+					usage(argv[0]);
+				else if(c == 'D')	
+					detach();
+				continue;
+			}
+		}
+	}
+	
+	//free calloc
+	free(long_options);	
+	parsed = true;
+#if 0
+	int c;
+	int option_index;
+
+	
 
 	while (true) {
-		c = getopt_long(argc, argv, "Dhd:b:s:k:r:f:c:i:p:a:", long_options, &option_index);
+		c = getopt_long(argc, argv, "Dhd:b:s:k:r:f:c:i:p:H:", long_options, &option_index);
 		if (c == -1)
 			break;
 
@@ -118,14 +252,41 @@ cunixenv::parse_args(
 			controller_port = atoi(optarg);
 			break;
 
-		case 'a':
-			enable_hal = atoi(optarg);
+		case 'H':
+			hw_driver = std::string(optarg);
 			break;
 		}
 	}
-
+#endif
 }
 
+void cunixenv::add_argument(carg arg){
+	arguments.push_back(arg);	
+}
+
+std::string cunixenv::get_arg(std::string name){
+	if(!parsed)
+		throw std::runtime_error("Args not yet parsed. use parse_args()");
+	std::vector<carg>::iterator it;
+	for(it = this->arguments.begin(); it != this->arguments.end(); it++){
+		if(it->full_name == name)
+			return it->current_value;
+	}
+	throw std::runtime_error("Unknown argument");
+}
+
+std::string cunixenv::get_arg(char shortcut){
+
+	if(!parsed)
+		throw std::runtime_error("Args not yet parsed. use parse_args()");
+	
+	std::vector<carg>::iterator it;
+	for(it = this->arguments.begin(); it != this->arguments.end(); it++){
+		if(it->shortcut == shortcut)
+			return it->current_value;
+	}
+	throw std::runtime_error("Unknown argument");
+}
 
 void
 cunixenv::detach()
@@ -143,8 +304,6 @@ cunixenv::detach()
 	}
 
 	// child code
-
-
 	if ((pid = setsid()) < 0) // detach from controlling terminal
 	{
 		fprintf(stderr, "setsid() sys-call failed: %d (%s)\n", errno, strerror(errno));
--- src/rofl/platform/unix/cunixenv.h
+++ src/rofl/platform/unix/cunixenv.h
@@ -20,41 +20,110 @@ extern "C" {
 }
 #endif
 
+#include <iostream>
 #include <string>
-
+#include <stdexcept>
 #include "csyslog.h"
 
+
+/*
+* Helper class to manipulate easily arguments in C++ programs. Argmuents are ALWAYS strings
+*/
+
+#define NO_ARGUMENT no_argument
+#define REQUIRED_ARGUMENT required_argument
+#define OPTIONAL_ARGUMENT optional_argument
+
+class carg
+{
+
+public:
+	bool optional;
+	int value_type;
+	char shortcut;
+	std::string full_name;
+	std::string description;
+	std::string default_value;
+	
+	/* Current state */
+	bool present;
+	std::string current_value;
+
+
+	/* Constructor */
+	carg(void){};
+
+	carg(bool optional, 
+		int value_type, 
+		char shortcut, 
+		std::string full_name, 
+		std::string description, 
+		std::string default_value);
+
+	/* Atempt to parse argument from optarg provided by getopt*/
+	std::string parse_argument(char* optarg);
+	bool is_present(void){return present;}
+	std::string value(void){return current_value;}
+};
+
+/* Forward declaration */
+
+/* 
+* Unix parser class 
+*/
 class cunixenv
 {
+private:
+/*
+	 int debug;			// verbosity
+	 int n_buffers;			// #buffers for cfwdelem
+	 int block_size; 		// block size in units of getpagesize() for cmmapport (default: 8)
+	 int n_blocks; 			// #blocks for cmmapport instances (default: 8)
+	 int frame_size; 		// frame size for cmmapport in bytes (default: 2048)
+	 int n_fwdengines;		// #fwdengines
+	 std::string controller_ip;	// ip address of the controller
+	 u_int16_t controller_port;	// port number of the controller
+	 std::string config_filename;
+	 std::string hw_driver;		//HAL driver 
+*/
+	bool parsed;
+	std::vector<carg> arguments;
+
+	/**
+	 * Usage method
+	 */
+	void usage(char *argv0);
+
+	/**
+	 * Fork process
+	 */
+	void detach(void);
+
+
 public:
 
-	static int debug;			// verbosity
-	static int n_buffers;		// #buffers for cfwdelem
-	static int block_size; 	// block size in units of getpagesize() for cmmapport (default: 8)
-	static int n_blocks; 		// #blocks for cmmapport instances (default: 8)
-	static int frame_size; 	// frame size for cmmapport in bytes (default: 2048)
-	static int n_fwdengines;	// #fwdengines
-	static std::string controller_ip;	// ip address of the controller
-	static u_int16_t controller_port;	// port number of the controller
-	static std::string config_filename;
-	static int enable_hal;		// enable the Hardware Abstraction Layer
+	/*
+	* Constructor 
+	*/
+	cunixenv(std::vector<carg>* args=NULL);
 
-public:
-
+	/*
+	* Add argument to current list of arguments to parse 
+	*/
+	void add_argument(carg arg);
+	
 	/**
-	 *
+	 * Parse arguments using getopt
 	 */
-	static void usage(char *argv0);
+	void parse_args(int argc, char** argv);
 
-	/**
-	 *
+	/*
+	 * Get value methods
 	 */
-	static void detach();
+	std::string get_arg(std::string name);	
 
-	/**
-	 *
-	 */
-	static void parse_args(int argc, char** argv);
+	std::string get_arg(char shortcut);	
+	
 };
 
 #endif
