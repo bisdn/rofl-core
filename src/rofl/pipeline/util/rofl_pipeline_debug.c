815a2a5b382287784f4d9629cc0b36b35da1eb69
Tobias Jungel <tobias.jungel@bisdn.de>
2013-01-28 14:20:16 +0100

    added comment

--- src/rofl/pipeline/util/rofl_pipeline_debug.c
+++ src/rofl/pipeline/util/rofl_pipeline_debug.c
@@ -10,4 +10,4 @@
 #include <stdio.h>
 
 int rofl_pipeline_debug_level[MAX_DEBUG_CLASS] = ROFL_DBG_DEFAULT;
-int (*rofl_pipeline_debug_print)(const char *format, ... ) = &printf;
+int (*rofl_pipeline_debug_print)(const char *format, ... ) = &printf; // todo move to the driver side
