6e1deca51a1bff4f19ed9c6e81f48c7214dcfa1c
Tobias Jungel <tobias.jungel@bisdn.de>
2013-01-28 14:16:51 +0100

    pipeline debugging facility

---
+++ src/rofl/pipeline/util/rofl_pipeline_debug.c
@@ -0,0 +1,13 @@
+/*
+ * rofl_pipeline_debug.c
+ *
+ *  Created on: Nov 12, 2012
+ *      Author: tobi
+ */
+
+#include "rofl_pipeline_debug.h"
+
+#include <stdio.h>
+
+int rofl_pipeline_debug_level[MAX_DEBUG_CLASS] = ROFL_DBG_DEFAULT;
+int (*rofl_pipeline_debug_print)(const char *format, ... ) = &printf;
