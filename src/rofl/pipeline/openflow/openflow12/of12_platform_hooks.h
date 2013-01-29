c30e9335e366435cf61e38d9aa8fc25473e35d74
Victor Alvarez <victor.alvarez@eict.de>
2013-01-29 12:41:38 +0100

    Added guards to header file

--- src/rofl/pipeline/openflow/openflow12/of12_platform_hooks.h
+++ src/rofl/pipeline/openflow/openflow12/of12_platform_hooks.h
@@ -3,6 +3,8 @@
  * wich platform must support and provide an implementation.
  * 
  */
+#ifndef OF12_PLATFORM_HOOKS_H_
+#define OF12_PLATFORM_HOOKS_H_
 
 //C++ extern C
 ROFL_PIPELINE_BEGIN_DECLS
@@ -10,3 +12,5 @@ ROFL_PIPELINE_BEGIN_DECLS
 unsigned int of12_platform_process_packet_in();
 
 ROFL_PIPELINE_END_DECLS
+
+#endif //OF12_PLATFORM_HOOKS_H_
