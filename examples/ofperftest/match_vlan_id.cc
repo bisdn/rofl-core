9c1448df5a9fd134687d4ec6fd3df1bb6a5da856
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-06-13 10:05:17 +0200

    dumdidum

---
+++ examples/ofperftest/match_vlan_id.cc
@@ -0,0 +1,287 @@
+#include "match_vlan_id.h"
+
+#include <inttypes.h>
+
+match_vlan_id::match_vlan_id(unsigned int n_entries) :
+	n_entries(n_entries),
+	fib_check_timeout(5), // check for expired FIB entries every 5 seconds
+	fm_delete_all_timeout(30)
+{
+	// ...
+	register_timer(ETHSWITCH_TIMER_FIB, fib_check_timeout);
+	register_timer(ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL, fm_delete_all_timeout);
+}
+
+
+
+match_vlan_id::~match_vlan_id()
+{
+	flow_mod_delete_all();
+}
+
+
+
+void
+match_vlan_id::handle_timeout(int opaque)
+{
+	switch (opaque) {
+	case ETHSWITCH_TIMER_FIB: {
+		drop_expired_fib_entries();
+	} break;
+	case ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL: {
+		//flow_mod_delete_all();
+	} break;
+	default:
+		crofbase::handle_timeout(opaque);
+	}
+}
+
+
+
+void
+match_vlan_id::drop_expired_fib_entries()
+{
+	// iterate over all FIB entries and delete expired ones ...
+
+	register_timer(ETHSWITCH_TIMER_FIB, fib_check_timeout);
+}
+
+
+
+void
+match_vlan_id::install_flow_mods(cofdpt *dpt, unsigned int n)
+{
+	if (0 == dpt) {
+		fprintf(stderr, "error installing test FlowMod entries on data path: dpt is NULL");
+		return;
+	}
+
+	// sanity check: data path should have at least two ports :)
+	if (dpt->get_ports().size() < 2) {
+		fprintf(stderr, "error installing test FlowMod entries on data path: less than 2 ports");
+		return;
+	}
+
+
+	crandom r;
+	uint64_t r_num = r.uint64();
+
+	fprintf(stderr, "installing %u fake FlowMod entries\n", n);
+
+	int vid = 1;
+
+	for (unsigned int i = 0; i < n; i++) {
+
+		cflowentry fe(dpt->get_version());
+
+		fe.set_command(OFPFC_ADD);
+		fe.set_buffer_id(OFP_NO_BUFFER);
+		fe.set_idle_timeout(0);
+		fe.set_hard_timeout(0);
+		fe.set_table_id(1);
+
+		r_num += n;
+		cmacaddr r_mac("00:00:00:00:00:00");
+		r_mac[0] = ((uint8_t*)&r_num)[0];
+		r_mac[1] = ((uint8_t*)&r_num)[1];
+		r_mac[2] = ((uint8_t*)&r_num)[2];
+		r_mac[3] = ((uint8_t*)&r_num)[3];
+		r_mac[4] = ((uint8_t*)&r_num)[4];
+		r_mac[5] = ((uint8_t*)&r_num)[5];
+
+		r_mac[0] &= 0xf7;
+
+		fe.match.set_eth_dst(r_mac);
+		fe.match.set_vlan_vid(vid);
+		++vid;
+		vid = (vid ==  100) ? 101 : vid; // skip VLAN id 100
+		vid = (vid == 4096) ?   1 : vid; // wrap around at 0xffff
+		fe.instructions.next() = cofinst_write_actions();	// do nothing, drop packet
+
+		fprintf(stderr, "match_vlan_id: calling FLOW-MOD with entry: %s\n",
+				fe.c_str());
+
+		send_flow_mod_message(
+				dpt,
+				fe);
+	}
+}
+
+
+
+void
+match_vlan_id::flow_mod_delete_all()
+{
+	std::map<cofdpt*, std::map<uint16_t, std::map<cmacaddr, struct fibentry_t> > >::iterator it;
+
+	for (it = fib.begin(); it != fib.end(); ++it) {
+		cofdpt *dpt = it->first;
+
+		cflowentry fe(dpt->get_version());
+		fe.set_command(OFPFC_DELETE);
+		fe.set_table_id(OFPTT_ALL);
+		fe.set_out_port(OFPP_ANY);
+		fe.set_out_group(OFPG_ANY);
+
+		fprintf(stderr, "FLOW-MOD: delete all: %s\n", fe.c_str());
+
+		send_flow_mod_message(dpt, fe);
+	}
+
+	register_timer(ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL, fm_delete_all_timeout);
+}
+
+
+
+void
+match_vlan_id::handle_dpath_open(
+		cofdpt *dpt)
+{
+	fib[dpt] = std::map<uint16_t, std::map<cmacaddr, struct fibentry_t> >();
+	// do nothing here
+
+	install_flow_mods(dpt, n_entries);
+}
+
+
+
+void
+match_vlan_id::handle_dpath_close(
+		cofdpt *dpt)
+{
+	fib.erase(dpt);
+}
+
+
+
+void
+match_vlan_id::handle_packet_in(
+		cofdpt *dpt,
+		cofmsg_packet_in *msg)
+{
+	cmacaddr eth_src = msg->get_packet().ether()->get_dl_src();
+
+	/*
+	 * sanity check: if source mac is multicast => invalid frame
+	 */
+	if (eth_src.is_multicast()) {
+		delete msg; return;
+	}
+
+	/*
+	 * block mac address 01:80:c2:00:00:00
+	 */
+	if (msg->get_packet().ether()->get_dl_dst() == cmacaddr("01:80:c2:00:00:00") ||
+		msg->get_packet().ether()->get_dl_dst() == cmacaddr("01:00:5e:00:00:fb")) {
+		cflowentry fe(dpt->get_version());
+
+		fe.set_command(OFPFC_ADD);
+		fe.set_buffer_id(msg->get_buffer_id());
+		fe.set_idle_timeout(15);
+		fe.set_table_id(msg->get_table_id());
+
+		fe.match.set_in_port(msg->get_match().get_in_port());
+		fe.match.set_eth_dst(msg->get_packet().ether()->get_dl_dst());
+		fe.instructions.next() = cofinst_apply_actions();
+
+		fprintf(stderr, "match_vlan_id: installing FLOW-MOD with entry: %s\n",
+				fe.c_str());
+
+		send_flow_mod_message(
+				dpt,
+				fe);
+
+		delete msg; return;
+	}
+
+	fprintf(stderr, "match_vlan_id: PACKET-IN from dpid:0x%"PRIu64" buffer-id:0x%x => from %s to %s type: 0x%x\n",
+			dpt->get_dpid(),
+			msg->get_buffer_id(),
+			msg->get_packet().ether()->get_dl_src().c_str(),
+			msg->get_packet().ether()->get_dl_dst().c_str(),
+			msg->get_packet().ether()->get_dl_type());
+
+	/*
+	 * get VLAN-ID and destination mac
+	 */
+	uint16_t vlan_id = 0xffff;
+	cmacaddr eth_dst = msg->get_packet().ether()->get_dl_dst();
+
+	try {
+		// vlan(): if no VLAN tag found => throws ePacketNotFound
+		vlan_id = msg->get_packet().vlan()->get_dl_vlan_id();
+	} catch (ePacketNotFound& e) {}
+
+
+
+	/*
+	 * if multicast or outgoing port unknown => FLOOD packet
+	 */
+	if (eth_dst.is_multicast() ||
+			(fib[dpt][vlan_id].find(eth_dst) == fib[dpt][vlan_id].end()))
+	{
+		cofaclist actions;
+		actions.next() = cofaction_output(OFPP_FLOOD);
+
+		if (OFP_NO_BUFFER == msg->get_buffer_id()) {
+			send_packet_out_message(
+					dpt,
+					msg->get_buffer_id(),
+					msg->get_match().get_in_port(),
+					actions,
+					msg->get_packet().soframe(), msg->get_packet().framelen());
+		} else {
+			send_packet_out_message(
+					dpt,
+					msg->get_buffer_id(),
+					msg->get_match().get_in_port(),
+					actions);
+		}
+
+		fprintf(stderr, "match_vlan_id: calling PACKET-OUT with ActionList: %s\n",
+				actions.c_str());
+
+	}
+	/*
+	 * unicast destination mac is known in FIB
+	 */
+	else
+	{
+		uint32_t out_port = fib[dpt][vlan_id][eth_dst].port_no;
+
+		if (msg->get_match().get_in_port() == out_port) {
+			delete msg; return;
+		}
+
+		cflowentry fe(dpt->get_version());
+
+		fe.set_command(OFPFC_ADD);
+		fe.set_buffer_id(msg->get_buffer_id());
+		fe.set_idle_timeout(0);
+		fe.set_table_id(msg->get_table_id());
+
+		fe.match.set_eth_dst(eth_dst);
+		if (vlan_id != 0xffff)
+			fe.match.set_vlan_vid(vlan_id);
+		fe.instructions.next() = cofinst_write_actions();
+		fe.instructions[0].actions.next() = cofaction_output(out_port);
+
+		fprintf(stderr, "match_vlan_id: calling FLOW-MOD with entry: %s\n",
+				fe.c_str());
+
+		send_flow_mod_message(
+				dpt,
+				fe);
+	}
+
+	/*
+	 * update FIB
+	 */
+	fib[dpt][vlan_id][eth_src].port_no = msg->get_match().get_in_port(); // may throw eOFmatchNotFound
+	fib[dpt][vlan_id][eth_src].timeout = time(NULL) + fib_check_timeout;
+
+	delete msg;
+}
+
+
+
---
+++ examples/ofperftest/match_vlan_id.h
@@ -0,0 +1,68 @@
+#ifndef MATCH_VLAN_ID_H
+#define MATCH_VLAN_ID_H 1
+
+#include <map>
+#include "rofl/common/cmacaddr.h"
+#include "rofl/common/caddress.h"
+#include "rofl/common/crofbase.h"
+#include "rofl/common/openflow/cofdpt.h"
+
+#include "ofperftest.h"
+
+using namespace rofl;
+
+class match_vlan_id :
+		public ofperftest
+{
+private:
+
+	struct fibentry_t {
+		uint32_t 		port_no;	// port where a certain is attached
+		time_t 			timeout;	// timeout event for this FIB entry
+	};
+
+	unsigned int 		n_entries;	// number of competing flowmods to be installed
+	// a very simple forwarding information base
+	std::map<cofdpt*, std::map<uint16_t, std::map<cmacaddr, struct fibentry_t> > > fib;
+
+	unsigned int 		fib_check_timeout; 		// periodic timeout for removing expired FIB entries
+	unsigned int		fm_delete_all_timeout;	// periodic purging of all FLOW-MODs
+
+	enum match_vlan_id_timer_t {
+		ETHSWITCH_TIMER_BASE = ((0x6271)),
+		ETHSWITCH_TIMER_FIB,
+		ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL,
+	};
+
+public:
+
+	match_vlan_id(unsigned int n_entries = 0);
+
+	virtual
+	~match_vlan_id();
+
+	virtual void
+	handle_timeout(int opaque);
+
+	virtual void
+	handle_dpath_open(cofdpt *dpt);
+
+	virtual void
+	handle_dpath_close(cofdpt *dpt);
+
+	virtual void
+	handle_packet_in(cofdpt *dpt, cofmsg_packet_in *msg);
+
+private:
+
+	void
+	install_flow_mods(cofdpt *dpt, unsigned int n = 0);
+
+	void
+	drop_expired_fib_entries();
+
+	void
+	flow_mod_delete_all();
+};
+
+#endif
