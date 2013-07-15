d161a0512b79e3b6a1a3285ede578d46a1d74a97
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-07-15 12:48:13 +0200

    rewrite of etherswitch example

--- examples/etherswitch/etherswitch.cc
+++ examples/etherswitch/etherswitch.cc
@@ -2,20 +2,16 @@
 
 #include <inttypes.h>
 
-etherswitch::etherswitch() :
-	fib_check_timeout(5), // check for expired FIB entries every 5 seconds
-	flow_stats_timeout(10),
-	fm_delete_all_timeout(30)
+using namespace etherswitch;
+
+ethswitch::ethswitch()
 {
-	// ...
-	register_timer(ETHSWITCH_TIMER_FIB, fib_check_timeout);
-	register_timer(ETHSWITCH_TIMER_FLOW_STATS, flow_stats_timeout);
-	register_timer(ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL, fm_delete_all_timeout);
+
 }
 
 
 
-etherswitch::~etherswitch()
+ethswitch::~ethswitch()
 {
 	// ...
 }
@@ -23,38 +19,18 @@ etherswitch::~etherswitch()
 
 
 void
-etherswitch::handle_timeout(int opaque)
+ethswitch::handle_timeout(int opaque)
 {
-	switch (opaque) {
-	case ETHSWITCH_TIMER_FIB: {
-		drop_expired_fib_entries();
-	} break;
-	case ETHSWITCH_TIMER_FLOW_STATS: {
-		request_flow_stats();
-	} break;
-	case ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL: {
-		//flow_mod_delete_all();
-	} break;
-	default:
-		crofbase::handle_timeout(opaque);
-	}
+
 }
 
 
 
-void
-etherswitch::drop_expired_fib_entries()
-{
-	// iterate over all FIB entries and delete expired ones ...
-
-	register_timer(ETHSWITCH_TIMER_FIB, fib_check_timeout);
-}
-
-
 
 void
-etherswitch::request_flow_stats()
+ethswitch::request_flow_stats()
 {
+#if 0
 	std::map<cofdpt*, std::map<uint16_t, std::map<cmacaddr, struct fibentry_t> > >::iterator it;
 
 	for (it = fib.begin(); it != fib.end(); ++it) {
@@ -92,13 +68,15 @@ etherswitch::request_flow_stats()
 	}
 
 	register_timer(ETHSWITCH_TIMER_FLOW_STATS, flow_stats_timeout);
+#endif
 }
 
 
 
 void
-etherswitch::handle_flow_stats_reply(cofdpt *dpt, cofmsg_flow_stats_reply *msg)
+ethswitch::handle_flow_stats_reply(cofdpt *dpt, cofmsg_flow_stats_reply *msg)
 {
+#if 0
 	if (fib.find(dpt) == fib.end()) {
 		delete msg; return;
 	}
@@ -121,61 +99,46 @@ etherswitch::handle_flow_stats_reply(cofdpt *dpt, cofmsg_flow_stats_reply *msg)
 		} break;
 		}
 	}
-
+#endif
 	delete msg;
 }
 
 
 
-void
-etherswitch::flow_mod_delete_all()
-{
-	std::map<cofdpt*, std::map<uint16_t, std::map<cmacaddr, struct fibentry_t> > >::iterator it;
-
-	for (it = fib.begin(); it != fib.end(); ++it) {
-		cofdpt *dpt = it->first;
-
-		cflowentry fe(dpt->get_version());
-		fe.set_command(OFPFC_DELETE);
-		fe.set_table_id(OFPTT_ALL);
-		fe.set_out_port(OFPP_ANY);
-		fe.set_out_group(OFPG_ANY);
-
-		fprintf(stderr, "FLOW-MOD: delete all: %s\n", fe.c_str());
-
-		send_flow_mod_message(dpt, fe);
-	}
-
-	register_timer(ETHSWITCH_TIMER_FLOW_MOD_DELETE_ALL, fm_delete_all_timeout);
-}
-
 
 
 void
-etherswitch::handle_dpath_open(
+ethswitch::handle_dpath_open(
 		cofdpt *dpt)
 {
+#if 0
 	fib[dpt] = std::map<uint16_t, std::map<cmacaddr, struct fibentry_t> >();
 	// do nothing here
+#endif
+	cfib::get_fib(dpt->get_dpid()).dpt_bind(this, dpt);
 }
 
 
 
 void
-etherswitch::handle_dpath_close(
+ethswitch::handle_dpath_close(
 		cofdpt *dpt)
 {
+	cfib::get_fib(dpt->get_dpid()).dpt_release(this, dpt);
+#if 0
 	fib.erase(dpt);
+#endif
 }
 
 
 
 void
-etherswitch::handle_packet_in(
+ethswitch::handle_packet_in(
 		cofdpt *dpt,
 		cofmsg_packet_in *msg)
 {
 	cmacaddr eth_src = msg->get_packet().ether()->get_dl_src();
+	cmacaddr eth_dst = msg->get_packet().ether()->get_dl_dst();
 
 	/*
 	 * sanity check: if source mac is multicast => invalid frame
@@ -217,84 +180,45 @@ etherswitch::handle_packet_in(
 			msg->get_packet().ether()->get_dl_dst().c_str(),
 			msg->get_packet().ether()->get_dl_type());
 
-	/*
-	 * get VLAN-ID and destination mac
-	 */
-	uint16_t vlan_id = 0xffff;
-	cmacaddr eth_dst = msg->get_packet().ether()->get_dl_dst();
 
-	try {
-		// vlan(): if no VLAN tag found => throws ePacketNotFound
-		vlan_id = msg->get_packet().vlan()->get_dl_vlan_id();
-	} catch (ePacketNotFound& e) {}
+	cofaclist actions;
 
+	if (eth_dst.is_multicast()) {
 
-
-	/*
-	 * if multicast or outgoing port unknown => FLOOD packet
-	 */
-	if (eth_dst.is_multicast() ||
-			(fib[dpt][vlan_id].find(eth_dst) == fib[dpt][vlan_id].end()))
-	{
-		cofaclist actions;
 		actions.next() = cofaction_output(OFPP_FLOOD);
 
-		if (OFP_NO_BUFFER == msg->get_buffer_id()) {
-			send_packet_out_message(
-					dpt,
-					msg->get_buffer_id(),
-					msg->get_match().get_in_port(),
-					actions,
-					msg->get_packet().soframe(), msg->get_packet().framelen());
-		} else {
-			send_packet_out_message(
-					dpt,
-					msg->get_buffer_id(),
-					msg->get_match().get_in_port(),
-					actions);
+	} else {
+
+		try {
+			cfibentry& entry = cfib::get_fib(dpt->get_dpid()).fib_lookup(
+						this,
+						dpt,
+						msg->get_packet().ether()->get_dl_dst(),
+						msg->get_packet().ether()->get_dl_src(),
+						msg->get_match().get_in_port());
+
+			if (msg->get_match().get_in_port() == entry.get_out_port_no()) {
+				delete msg; return;
+			}
+
+			actions.next() = cofaction_output(entry.get_out_port_no());
+
+		} catch (eFibNotFound& e) {
+
+			actions.next() = cofaction_output(OFPP_FLOOD);
 		}
-
-		fprintf(stderr, "etherswitch: calling PACKET-OUT with ActionList: %s\n",
-				actions.c_str());
-
-	}
-	/*
-	 * unicast destination mac is known in FIB
-	 */
-	else
-	{
-		uint32_t out_port = fib[dpt][vlan_id][eth_dst].port_no;
-
-		if (msg->get_match().get_in_port() == out_port) {
-			delete msg; return;
-		}
-
-		cflowentry fe(dpt->get_version());
-
-		fe.set_command(OFPFC_ADD);
-		fe.set_buffer_id(msg->get_buffer_id());
-		fe.set_idle_timeout(15);
-		fe.set_table_id(msg->get_table_id());
-
-		fe.match.set_eth_dst(eth_dst);
-		fe.instructions.next() = cofinst_write_actions();
-		fe.instructions[0].actions.next() = cofaction_output(out_port);
-
-		fprintf(stderr, "etherswitch: calling FLOW-MOD with entry: %s\n",
-				fe.c_str());
-
-		send_flow_mod_message(
-				dpt,
-				fe);
 	}
 
-	/*
-	 * update FIB
-	 */
-	fib[dpt][vlan_id][eth_src].port_no = msg->get_match().get_in_port(); // may throw eOFmatchNotFound
-	fib[dpt][vlan_id][eth_src].timeout = time(NULL) + fib_check_timeout;
 
-	delete msg;
+
+	if (OFP_NO_BUFFER != msg->get_buffer_id()) {
+		send_packet_out_message(dpt, msg->get_buffer_id(), msg->get_match().get_in_port(), actions);
+	} else {
+		send_packet_out_message(dpt, msg->get_buffer_id(), msg->get_match().get_in_port(), actions,
+				msg->get_packet().soframe(), msg->get_packet().framelen());
+	}
+
+	delete msg; return;
 }
 
 
