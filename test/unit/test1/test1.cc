92d7ab0eca5c5157d8eb5bdfdff4293d502e5280
Victor Alvarez <victor.alvarez@bisdn.de>
2013-10-09 14:03:25 +0200

    fixed test1

--- test/unit/test1/test1.cc
+++ test/unit/test1/test1.cc
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <rofl/controller/control/cfwdelem.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int args, char** argv){
 	
