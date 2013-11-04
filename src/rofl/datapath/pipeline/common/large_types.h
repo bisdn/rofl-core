a6672417d8aac77820cf131b7864058a71c593fc
Victor Alvarez <victor.alvarez@bisdn.de>
2013-11-04 15:47:02 +0100

    fixing SWAP_128. Now high and low parts do not change position

--- src/rofl/datapath/pipeline/common/large_types.h
+++ src/rofl/datapath/pipeline/common/large_types.h
@@ -24,10 +24,8 @@ typedef struct wrap_u128{
 #define UINT128__T_LO(x) ((w128_t*)&x)->lo
 
 #define SWAP_U128(x) do{ \
-	uint64_t tmp = ((w128_t*)&x)->hi; \
-	((w128_t*)&x)->hi = __bswap_64 (((w128_t*)&x)->lo); \
-	((w128_t*)&x)->lo = __bswap_64 (tmp); \
+	((w128_t*)&x)->hi = __bswap_64 (((w128_t*)&x)->hi); \
+	((w128_t*)&x)->lo = __bswap_64 (((w128_t*)&x)->lo); \
 }while(0)
-
 
 #endif //__LARGE_TYPES_H__
