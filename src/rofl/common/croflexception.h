5f5520d924e7d07b93b6809058cb5d1a0b07c479
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-12-27 16:17:29 +0100

    croflexception => define

--- src/rofl/common/croflexception.h
+++ src/rofl/common/croflexception.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef CERROR_H
-#define CERROR_H
+#ifndef CROFLEXCEPTION_H
+#define CROFLEXCEPTION_H
 
 #include <set>
 #include <string>
