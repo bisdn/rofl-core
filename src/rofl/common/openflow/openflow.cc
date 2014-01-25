2b8604c599f39d09c00c46304d59bb9f97779846
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-01-25 19:31:11 +0100

    openflow.h => added class rofl::openflow::base for collecting static auxiliary methods

--- src/rofl/common/openflow/openflow.cc
+++ src/rofl/common/openflow/openflow.cc
@@ -5,52 +5,55 @@
  *      Author: andreas
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <inttypes.h>
-#ifdef __cplusplus
-}
-#endif
-
 #include "rofl/common/openflow/openflow.h"
-#include "rofl/common/croflexception.h"
 
 using namespace rofl::openflow;
 
-
 uint32_t
-get_ofp_no_buffer(uint8_t ofp_version) {
+base::get_ofp_no_buffer(uint8_t ofp_version) {
 
 	switch (ofp_version) {
 	case rofl::openflow10::OFP_VERSION: return rofl::openflow10::OFP_NO_BUFFER; break;
 	case rofl::openflow12::OFP_VERSION: return rofl::openflow12::OFP_NO_BUFFER; break;
 	case rofl::openflow13::OFP_VERSION: return rofl::openflow13::OFP_NO_BUFFER; break;
-	default: throw rofl::eBadVersion();
+	default: return 0;
 	}
 }
 
 
 uint32_t
-get_ofp_flood_port(uint8_t ofp_version) {
+base::get_ofpp_flood_port(uint8_t ofp_version) {
 
 	switch (ofp_version) {
 	case rofl::openflow10::OFP_VERSION: return rofl::openflow10::OFPP_FLOOD; break;
 	case rofl::openflow12::OFP_VERSION: return rofl::openflow12::OFPP_FLOOD; break;
 	case rofl::openflow13::OFP_VERSION: return rofl::openflow13::OFPP_FLOOD; break;
-	default: throw rofl::eBadVersion();
+	default: return 0;
 	}
 }
 
 
 uint32_t
-get_ofp_controller_port(uint8_t ofp_version) {
+base::get_ofpp_controller_port(uint8_t ofp_version) {
 
 	switch (ofp_version) {
 	case rofl::openflow10::OFP_VERSION: return rofl::openflow10::OFPP_CONTROLLER; break;
 	case rofl::openflow12::OFP_VERSION: return rofl::openflow12::OFPP_CONTROLLER; break;
 	case rofl::openflow13::OFP_VERSION: return rofl::openflow13::OFPP_CONTROLLER; break;
-	default: throw rofl::eBadVersion();
+	default: return 0;
+	}
+}
+
+
+
+uint8_t
+base::get_ofptt_all(uint8_t ofp_version) {
+
+	switch (ofp_version) {
+	case rofl::openflow10::OFP_VERSION: return rofl::openflow10::OFPTT_ALL; break;
+	case rofl::openflow12::OFP_VERSION: return rofl::openflow12::OFPTT_ALL; break;
+	case rofl::openflow13::OFP_VERSION: return rofl::openflow13::OFPTT_ALL; break;
+	default: return 0;
 	}
 }
 
