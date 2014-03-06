52a8e688f3abf80f5fee5eca25f430ae320128b1
Marc Sune <marc.sune@bisdn.de>
2014-03-06 12:46:40 +0100

    Moving librofl_is_present() to rofl.c

--- src/rofl/common/crofbase.cc
+++ src/rofl/common/crofbase.cc
@@ -2,10 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-extern "C" {
-// autoconf AC_CHECK_LIB helper function as C-declaration
-void librofl_is_present(void) {};
-}
+
 
 #include "crofbase.h"
 
