4a212836cd4fade6a5be4bcc4e8ddbd9e8b73a8a
Marc Sune <marc.sune@bisdn.de>
2014-03-13 23:07:33 +0100

    Adding byte swapping macros for non byteswap.h compliant systems

--- src/rofl/datapath/pipeline/common/endianness.h
+++ src/rofl/datapath/pipeline/common/endianness.h
@@ -35,10 +35,25 @@
 		#define NTOHB32(x) __bswap_32(x)
 		#define NTOHB64(x) __bswap_64(x)
 	#else
-		//TODO: maybe add a non-assembly impl?
-		#error byteswap.h not available; configure error?
-	#endif
+		//Host -> Network
+		#define HTONB16(x) ( (((x) >> 8) & 0x00FF) | (((x) << 8) & 0xFF00) )
+		#define HTONB32(x) \
+			( (((x) >> 24) & 0x000000FF) | (((x) >>  8) & 0x0000FF00) | \
+			(((x) <<  8) & 0x00FF0000) | (((x) << 24) & 0xFF000000) )
 
+		 
+		#define HTONB64(x) \
+			( (((x) >> 56) & 0x00000000000000FF) | (((x) >> 40) & 0x000000000000FF00) | \
+			(((x) >> 24) & 0x0000000000FF0000) | (((x) >>  8) & 0x00000000FF000000) | \
+			(((x) <<  8) & 0x000000FF00000000) | (((x) << 24) & 0x0000FF0000000000) | \
+			(((x) << 40) & 0x00FF000000000000) | (((x) << 56) & 0xFF00000000000000) )
+		
+		//Network -> Host
+		#define NTOHB16(x) HTONB16(x)
+		#define NTOHB32(x) HTONB32(x)
+		#define NTOHB64(x) HTONB64(x)
+	
+	#endif
 #else
 	#error Unsupported endianness
 #endif //endianness check
