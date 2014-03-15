3c74dff7b59b9e09e850781be1ebd2c959aff608
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-03-15 07:29:21 +0100

    git add cofflowstatsarray.* coftablestatsarray.*

---
+++ src/rofl/common/openflow/cofflowstatsarray.cc
@@ -0,0 +1,207 @@
+/*
+ * cofflows.cc
+ *
+ *  Created on: 14.03.2014
+ *      Author: andreas
+ */
+
+#include "rofl/common/openflow/cofflowstatsarray.h"
+
+using namespace rofl::openflow;
+
+
+cofflowstatsarray::cofflowstatsarray(uint8_t ofp_version) :
+		ofp_version(ofp_version)
+{
+
+}
+
+
+cofflowstatsarray::~cofflowstatsarray()
+{
+
+}
+
+
+cofflowstatsarray::cofflowstatsarray(cofflowstatsarray const& flows)
+{
+	*this = flows;
+}
+
+
+cofflowstatsarray&
+cofflowstatsarray::operator= (cofflowstatsarray const& flows)
+{
+	if (this == &flows)
+		return *this;
+
+	this->array.clear();
+
+	ofp_version = flows.ofp_version;
+	for (std::map<uint32_t, cofflow_stats_reply>::const_iterator
+			it = flows.array.begin(); it != flows.array.end(); ++it) {
+		this->array[it->first] = it->second;
+	}
+
+	return *this;
+}
+
+
+
+cofflowstatsarray&
+cofflowstatsarray::operator+= (cofflowstatsarray const& flows)
+{
+	/*
+	 * this may replace existing flow descriptions
+	 */
+	for (std::map<uint32_t, cofflow_stats_reply>::const_iterator
+			it = flows.array.begin(); it != flows.array.end(); ++it) {
+		this->array[it->first] = it->second;
+	}
+
+	return *this;
+}
+
+
+
+size_t
+cofflowstatsarray::length() const
+{
+	size_t len = 0;
+	for (std::map<uint32_t, cofflow_stats_reply>::const_iterator
+			it = array.begin(); it != array.end(); ++it) {
+		len += it->second.length();
+	}
+	return len;
+}
+
+
+
+void
+cofflowstatsarray::pack(uint8_t *buf, size_t buflen)
+{
+	if ((0 == buf) || (0 == buflen))
+		return;
+
+	if (buflen < length())
+		throw eInval();
+
+	switch (ofp_version) {
+	case rofl::openflow10::OFP_VERSION:
+	case rofl::openflow12::OFP_VERSION:
+	case rofl::openflow13::OFP_VERSION: {
+
+		for (std::map<uint32_t, cofflow_stats_reply>::iterator
+				it = array.begin(); it != array.end(); ++it) {
+			it->second.pack(buf, it->second.length());
+			buf += it->second.length();
+		}
+
+	} break;
+	default:
+		throw eBadVersion();
+	}
+}
+
+
+
+void
+cofflowstatsarray::unpack(uint8_t *buf, size_t buflen)
+{
+	array.clear();
+
+	switch (ofp_version) {
+	case rofl::openflow10::OFP_VERSION: {
+
+		uint32_t flow_id = 0;
+
+		while (buflen >= sizeof(struct rofl::openflow10::ofp_flow_stats)) {
+
+			add_flow_stats(flow_id++).unpack(buf, sizeof(struct rofl::openflow10::ofp_flow_stats));
+
+			buf += sizeof(struct rofl::openflow10::ofp_flow_stats);
+			buflen -= sizeof(struct rofl::openflow10::ofp_flow_stats);
+		}
+	} break;
+	case rofl::openflow12::OFP_VERSION: {
+
+		uint32_t flow_id = 0;
+
+		while (buflen >= sizeof(struct rofl::openflow12::ofp_flow_stats)) {
+
+			add_flow_stats(flow_id++).unpack(buf, sizeof(struct rofl::openflow12::ofp_flow_stats));
+
+			buf += sizeof(struct rofl::openflow12::ofp_flow_stats);
+			buflen -= sizeof(struct rofl::openflow12::ofp_flow_stats);
+		}
+	} break;
+	case rofl::openflow13::OFP_VERSION: {
+
+		uint32_t flow_id = 0;
+
+		while (buflen >= sizeof(struct rofl::openflow13::ofp_flow_stats)) {
+
+			add_flow_stats(flow_id++).unpack(buf, sizeof(struct rofl::openflow13::ofp_flow_stats));
+
+			buf += sizeof(struct rofl::openflow13::ofp_flow_stats);
+			buflen -= sizeof(struct rofl::openflow13::ofp_flow_stats);
+		}
+	} break;
+	default:
+		throw eBadRequestBadVersion();
+	}
+}
+
+
+
+rofl::cofflow_stats_reply&
+cofflowstatsarray::add_flow_stats(uint32_t flow_id)
+{
+	if (array.find(flow_id) != array.end()) {
+		array.erase(flow_id);
+	}
+	return (array[flow_id] = cofflow_stats_reply(ofp_version));
+}
+
+
+
+void
+cofflowstatsarray::drop_flow_stats(uint32_t flow_id)
+{
+	if (array.find(flow_id) == array.end()) {
+		return;
+	}
+	array.erase(flow_id);
+}
+
+
+
+rofl::cofflow_stats_reply&
+cofflowstatsarray::set_flow_stats(uint32_t flow_id)
+{
+	return (array[flow_id] = cofflow_stats_reply(ofp_version));
+}
+
+
+
+rofl::cofflow_stats_reply const&
+cofflowstatsarray::get_flow_stats(uint32_t flow_id)
+{
+	if (array.find(flow_id) == array.end()) {
+		throw;
+	}
+	return array.at(flow_id);
+}
+
+
+
+bool
+cofflowstatsarray::has_flow_stats(uint32_t flow_id)
+{
+	return (not (array.find(flow_id) == array.end()));
+}
+
+
+
+
+
---
+++ src/rofl/common/openflow/cofflowstatsarray.h
@@ -0,0 +1,133 @@
+/*
+ * cofflows.h
+ *
+ *  Created on: 14.03.2014
+ *      Author: andreas
+ */
+
+#ifndef COFFLOWSTATSARRAY_H_
+#define COFFLOWSTATSARRAY_H_
+
+#include <iostream>
+#include <map>
+
+#include "rofl/common/openflow/cofflowstats.h"
+
+namespace rofl {
+namespace openflow {
+
+class cofflowstatsarray {
+
+	uint8_t										ofp_version;
+	std::map<uint32_t, cofflow_stats_reply>		array;
+
+public:
+
+	/**
+	 *
+	 */
+	cofflowstatsarray(uint8_t ofp_version = OFP_VERSION_UNKNOWN);
+
+	/**
+	 *
+	 */
+	virtual
+	~cofflowstatsarray();
+
+	/**
+	 *
+	 */
+	cofflowstatsarray(cofflowstatsarray const& flows);
+
+	/**
+	 *
+	 */
+	cofflowstatsarray&
+	operator= (cofflowstatsarray const& flows);
+
+	/**
+	 *
+	 */
+	cofflowstatsarray&
+	operator+= (cofflowstatsarray const& flows);
+
+public:
+
+	/** returns length of packet in packed state
+	 *
+	 */
+	virtual size_t
+	length() const;
+
+	/**
+	 *
+	 */
+	virtual void
+	pack(uint8_t *buf = (uint8_t*)0, size_t buflen = 0);
+
+	/**
+	 *
+	 */
+	virtual void
+	unpack(uint8_t *buf, size_t buflen);
+
+public:
+
+	/**
+	 *
+	 */
+	void
+	clear() { array.clear(); };
+
+public:
+
+	/**
+	 *
+	 */
+	cofflow_stats_reply&
+	add_flow_stats(uint32_t flow_id);
+
+	/**
+	 *
+	 */
+	void
+	drop_flow_stats(uint32_t flow_id);
+
+	/**
+	 *
+	 */
+	cofflow_stats_reply&
+	set_flow_stats(uint32_t flow_id);
+
+	/**
+	 *
+	 */
+	cofflow_stats_reply const&
+	get_flow_stats(uint32_t flow_id);
+
+	/**
+	 *
+	 */
+	bool
+	has_flow_stats(uint32_t flow_id);
+
+public:
+
+	friend std::ostream&
+	operator<< (std::ostream& os, cofflowstatsarray const& flowstatsarray) {
+		os << "<cofflowstatsarray #flows:" << (int)flowstatsarray.array.size() << " >" << std::endl;
+		rofl::indent i(2);
+		for (std::map<uint32_t, cofflow_stats_reply>::const_iterator
+				it = flowstatsarray.array.begin(); it != flowstatsarray.array.end(); ++it) {
+			os << it->second;
+		}
+		return os;
+	}
+};
+
+}; // end of openflow
+}; // end of rofl
+
+
+
+#endif /* COFGROUPS_H_ */
