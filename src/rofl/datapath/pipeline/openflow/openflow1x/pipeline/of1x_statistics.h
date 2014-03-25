16a0426bb3bfb3954ff26069617e798fb69367f8
Marc Sune <marc.sune@bisdn.de>
2014-03-26 00:14:02 +0100

    Cleaning up of1x_statistics dead code

--- src/rofl/datapath/pipeline/openflow/openflow1x/pipeline/of1x_statistics.h
+++ src/rofl/datapath/pipeline/openflow/openflow1x/pipeline/of1x_statistics.h
@@ -105,20 +105,6 @@ typedef struct of1x_stats_port{
 	uint64_t tx_packets;	/* Number of transmitted packets. */
 	uint64_t rx_bytes;		/* Number of received bytes. */
 	uint64_t tx_bytes;		/* Number of transmitted bytes. */
-	//uint64_t rx_dropped;	/* Number of packets dropped by RX. */
-	//uint64_t tx_dropped;	/* Number of packets dropped by TX. */
-	/*uint64_t rx_errors;		// Number of receive errors. This is a super-set
-								of more specific receive errors and should be
-								greater than or equal to the sum of all
-								rx_*_err values. */
-	/*uint64_t tx_errors;		// Number of transmit errors. This is a super-set
-								of more specific transmit errors and should be
-								greater than or equal to the sum of all
-								tx_*_err values (none currently defined.) */
-	//uint64_t rx_frame_err;	/* Number of frame alignment errors. */
-	//uint64_t rx_over_err;	/* Number of packets with RX overrun. */
-	//uint64_t rx_crc_err;	/* Number of CRC errors. */
-	//uint64_t collisions;	/* Number of collisions. */
 	
 	platform_mutex_t* mutex; /*mutual exclusion to update the statistics*/
 }of1x_stats_port_t;
