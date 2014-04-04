cb0d258a141da07830735aa7f3b385854eb8d932
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-04-04 18:21:15 +0200

    renaming csocket_plain to csocket_impl

--- src/rofl/common/csocket.cc
+++ src/rofl/common/csocket.cc
@@ -1,5 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "rofl/common/csocket.h"
-#include "rofl/common/csocket_plain.h"
+#include "rofl/common/csocket_impl.h"
 #ifdef ROFL_HAVE_OPENSSL
 #include "rofl/common/csocket_openssl.h"
 #endif
--- src/rofl/common/csocket_impl.cc
+++ src/rofl/common/csocket_impl.cc
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "csocket_plain.h"
+#include "csocket_impl.h"
 
 
 using namespace rofl;
