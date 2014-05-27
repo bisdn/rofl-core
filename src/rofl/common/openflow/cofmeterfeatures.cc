49a60d80415e95191854ae9cb74c0a4887e7310f
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-05-27 13:58:06 +0200

    new class: cofmeterfeatures / new test: cofmeterfeatures_test

---
+++ src/rofl/common/openflow/cofmeterfeatures.cc
@@ -0,0 +1,166 @@
+#include "rofl/common/openflow/cofmeterfeatures.h"
+
+using namespace rofl::openflow;
+
+
+cofmeter_features_reply::cofmeter_features_reply(
+		uint8_t of_version,
+		uint8_t *buf,
+		size_t buflen) :
+				of_version(of_version),
+				max_meter(0),
+				band_types(0),
+				capabilities(0),
+				max_bands(0),
+				max_color(0)
+{
+	if ((buflen > 0) && (0 != buf)) {
+		unpack(buf, buflen);
+	}
+}
+
+
+
+cofmeter_features_reply::cofmeter_features_reply(
+		uint8_t of_version,
+		uint32_t max_meter,
+		uint32_t band_types,
+		uint32_t capabilities,
+		uint8_t max_bands,
+		uint8_t max_color) :
+				of_version(of_version),
+				max_meter(max_meter),
+				band_types(band_types),
+				capabilities(capabilities),
+				max_bands(max_bands),
+				max_color(max_color)
+{}
+
+
+
+cofmeter_features_reply::~cofmeter_features_reply()
+{}
+
+
+
+cofmeter_features_reply::cofmeter_features_reply(
+		const cofmeter_features_reply& meter_config)
+{
+	*this = meter_config;
+}
+
+
+
+cofmeter_features_reply&
+cofmeter_features_reply::operator= (
+		const cofmeter_features_reply& reply)
+{
+	if (this == &reply)
+		return *this;
+
+	of_version 		= reply.of_version;
+	max_meter		= reply.max_meter;
+	band_types		= reply.band_types;
+	capabilities	= reply.capabilities;
+	max_bands		= reply.max_bands;
+	max_color		= reply.max_color;
+
+	return *this;
+}
+
+
+
+bool
+cofmeter_features_reply::operator== (
+		cofmeter_features_reply const& reply)
+{
+	return ((of_version			== reply.of_version) &&
+			(max_meter			== reply.max_meter) &&
+			(band_types			== reply.band_types) &&
+			(capabilities		== reply.capabilities) &&
+			(max_bands			== reply.max_bands) &&
+			(max_color			== reply.max_color));
+}
+
+
+
+void
+cofmeter_features_reply::clear()
+{
+	max_meter		= 0;
+	band_types		= 0;
+	capabilities	= 0;
+	max_bands		= 0;
+	max_color		= 0;
+}
+
+
+
+size_t
+cofmeter_features_reply::length() const
+{
+	switch (of_version) {
+	case rofl::openflow13::OFP_VERSION: {
+		return (sizeof(struct rofl::openflow13::ofp_meter_features));
+	} break;
+	default:
+		throw eBadVersion();
+	}
+	return 0;
+}
+
+
+
+void
+cofmeter_features_reply::pack(uint8_t *buf, size_t buflen)
+{
+	if ((0 == buf) || (0 == buflen))
+		return;
+
+	switch (of_version) {
+	case rofl::openflow13::OFP_VERSION: {
+		if (buflen < length())
+			throw eInval();
+
+		struct rofl::openflow13::ofp_meter_features *meter_features =
+				(struct rofl::openflow13::ofp_meter_features*)buf;
+
+		meter_features->max_meter		= htobe32(max_meter);
+		meter_features->band_types		= htobe32(band_types);
+		meter_features->capabilities	= htobe32(capabilities);
+		meter_features->max_bands		= max_bands;
+		meter_features->max_color		= max_color;
+
+	} break;
+	default:
+		throw eBadVersion();
+	}
+}
+
+
+
+void
+cofmeter_features_reply::unpack(uint8_t *buf, size_t buflen)
+{
+	switch (of_version) {
+	case rofl::openflow13::OFP_VERSION: {
+
+		if (buflen < length())
+			throw eInval();
+
+		struct rofl::openflow13::ofp_meter_features* meter_features =
+				(struct rofl::openflow13::ofp_meter_features*)buf;
+
+		max_meter		= be32toh(meter_features->max_meter);
+		band_types		= be32toh(meter_features->band_types);
+		capabilities	= be32toh(meter_features->capabilities);
+		max_bands		= meter_features->max_bands;
+		max_color		= meter_features->max_color;
+
+	} break;
+	default:
+		throw eBadVersion();
+	}
+}
+
+
---
+++ src/rofl/common/openflow/cofmeterfeatures.h
@@ -0,0 +1,205 @@
+/*
+ * cofmeterfeatures.h
+ *
+ *  Created on: 26.05.2014
+ *      Author: andi
+ */
+
+#ifndef COFMETERFEATURES_H_
+#define COFMETERFEATURES_H_ 1
+
+#include "rofl/common/cmemory.h"
+#include "rofl/common/openflow/openflow.h"
+#include "rofl/common/openflow/openflow_rofl_exceptions.h"
+
+namespace rofl {
+namespace openflow {
+
+class eRofMeterConfigBase		: public RoflException {};
+class eRofMeterConfigInval		: public eRofMeterConfigBase {};
+class eRofMeterConfigNotFound	: public eRofMeterConfigBase {};
+
+class cofmeter_features_reply
+{
+public:
+	/**
+	 *
+	 */
+	cofmeter_features_reply(
+			uint8_t of_version = 0,
+			uint8_t *buf = (uint8_t*)0,
+			size_t buflen = 0);
+
+
+	/**
+	 *
+	 */
+	cofmeter_features_reply(
+			uint8_t of_version,
+			uint32_t max_meter,
+			uint32_t band_types,
+			uint32_t capabilities,
+			uint8_t max_bands,
+			uint8_t max_color);
+
+
+
+	/**
+	 *
+	 */
+	virtual
+	~cofmeter_features_reply();
+
+
+	/**
+	 *
+	 */
+	cofmeter_features_reply(
+			const cofmeter_features_reply& meter_features_reply);
+
+	/**
+	 *
+	 */
+	cofmeter_features_reply&
+	operator= (
+			const cofmeter_features_reply& meter_features_reply);
+
+	/**
+	 *
+	 */
+	bool
+	operator== (
+			const cofmeter_features_reply& meter_features_reply);
+
+public:
+
+	/**
+	 *
+	 */
+	void
+	clear();
+
+	/**
+	 *
+	 */
+	size_t
+	length() const;
+
+	/**
+	 *
+	 */
+	void
+	pack(uint8_t *buf, size_t buflen);
+
+	/**
+	 *
+	 */
+	void
+	unpack(uint8_t *buf, size_t buflen);
+
+public:
+
+	/**
+	 *
+	 */
+	void
+	set_version(uint8_t of_version) { this->of_version = of_version; };
+
+	/**
+	 *
+	 */
+	uint8_t
+	get_version() const { return of_version; };
+
+	/**
+	 *
+	 */
+	void
+	set_max_meter(uint32_t max_meter) { this->max_meter = max_meter; };
+
+	/**
+	 *
+	 */
+	uint32_t
+	get_max_meter() const { return max_meter; };
+
+	/**
+	 *
+	 */
+	void
+	set_band_types(uint32_t band_types) { this->band_types = band_types; };
+
+	/**
+	 *
+	 */
+	uint32_t
+	get_band_types() const { return band_types; };
+
+	/**
+	 *
+	 */
+	void
+	set_capabilities(uint32_t capabilities) { this->capabilities = capabilities; };
+
+	/**
+	 *
+	 */
+	uint32_t
+	get_capabilities() const { return capabilities; };
+
+	/**
+	 *
+	 */
+	void
+	set_max_bands(uint8_t max_bands) { this->max_bands = max_bands; };
+
+	/**
+	 *
+	 */
+	uint8_t
+	get_max_bands() const { return max_bands; };
+
+	/**
+	 *
+	 */
+	void
+	set_max_color(uint8_t max_color) { this->max_color = max_color; };
+
+	/**
+	 *
+	 */
+	uint8_t
+	get_max_color() const { return max_color; };
+
+
+public:
+
+	friend std::ostream&
+	operator<< (std::ostream& os, cofmeter_features_reply const& r) {
+		os << indent(0) << "<cofmeter_features_reply ofp-version: " << (int)r.get_version() << " >" << std::endl;
+		os << std::hex;
+		os << indent(2) << "<max-meter: 0x"			<< (unsigned int)r.get_max_meter() << " >" << std::endl;
+		os << indent(2) << "<band-types: 0x" 		<< (unsigned int)r.get_band_types() << " >" << std::endl;
+		os << indent(2) << "<capabilities: 0x" 		<< (unsigned int)r.get_capabilities() << " >" << std::endl;
+		os << indent(2) << "<max-bands: 0x" 		<< (unsigned int)r.get_max_bands() << " >" << std::endl;
+		os << indent(2) << "<max-color: 0x" 		<< (unsigned int)r.get_max_color() << " >" << std::endl;
+		os << std::dec;
+		return os;
+	};
+
+private: // data structures
+
+	uint8_t 						of_version;
+	uint32_t 						max_meter;
+	uint32_t						band_types;
+	uint32_t						capabilities;
+	uint8_t							max_bands;
+	uint8_t							max_color;
+};
+
+typedef cofmeter_features_reply cofmeter_features;
+
+} /* end of namespace openflow */
+} /* end of namespace rofl */
+
+#endif /* COFMETERFEATURES_H_ */
