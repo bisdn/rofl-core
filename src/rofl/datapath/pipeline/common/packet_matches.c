6d0209936604ab2527bd7d9aee4f56a92f566aaf
Marc Sune <marc.sune@bisdn.de>
2015-01-23 11:00:11 +0100

    Removing trailing chars

--- src/rofl/datapath/pipeline/common/packet_matches.c
+++ src/rofl/datapath/pipeline/common/packet_matches.c
@@ -8,8 +8,8 @@
 
 //FIXME: unify packet matches alignment with MATCHEs and ACTIONs; there should be only one piece of code dealing with alignment
 
-/* 
-* DEBUG/INFO dumping routines 
+/*
+* DEBUG/INFO dumping routines
 */
 //Fill pkt matches structure
 void fill_packet_matches(datapacket_t *const pkt, packet_matches_t* m){
@@ -19,24 +19,24 @@ void fill_packet_matches(datapacket_t *const pkt, packet_matches_t* m){
 	uint64_t* ptr64;
 	uint128__t* ptr128;
 	uint128__t zero_u128 = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
-	
+
 	//Packet size
-	m->__pkt_size_bytes = platform_packet_get_size_bytes(pkt);	
+	m->__pkt_size_bytes = platform_packet_get_size_bytes(pkt);
 
 	//Ports
 	m->__port_in = ( (ptr32=platform_packet_get_port_in(pkt))==NULL ? 0 : *ptr32);
 	m->__phy_port_in = ( (ptr32=platform_packet_get_phy_port_in(pkt))==NULL ? 0 : *ptr32);
-	
+
 	//Associated metadata
-	m->__metadata = pkt->__metadata; 
- 
+	m->__metadata = pkt->__metadata;
+
 	//802
 	m->__eth_dst = ( (ptr64=platform_packet_get_eth_dst(pkt))==NULL ? 0 : *ptr64);
 	m->__eth_src = ( (ptr64=platform_packet_get_eth_src(pkt))==NULL ? 0 : *ptr64);
 	m->__eth_type = ( (ptr16=platform_packet_get_eth_type(pkt))==NULL ? 0 : *ptr16);
-	
+
 	//802.1q VLAN outermost tag
-	m->__has_vlan = platform_packet_has_vlan(pkt);		
+	m->__has_vlan = platform_packet_has_vlan(pkt);
 	m->__vlan_vid = ( (ptr16=platform_packet_get_vlan_vid(pkt))==NULL ? 0 : *ptr16);
 	m->__vlan_pcp = ( (ptr8=platform_packet_get_vlan_pcp(pkt))==NULL ? 0 : *ptr8);
 
@@ -49,9 +49,9 @@ void fill_packet_matches(datapacket_t *const pkt, packet_matches_t* m){
 
 	//IP
 	m->__ip_proto = ( (ptr8=platform_packet_get_ip_proto(pkt))==NULL ? 0 : *ptr8);
-	m->__ip_dscp = platform_packet_get_ip_dscp(pkt);		
-	m->__ip_ecn = platform_packet_get_ip_ecn(pkt);			
-	
+	m->__ip_dscp = platform_packet_get_ip_dscp(pkt);
+	m->__ip_ecn = platform_packet_get_ip_ecn(pkt);
+
 	//IPv4
 	m->__ipv4_src = ( (ptr32=platform_packet_get_ipv4_src(pkt))==NULL ? 0 : *ptr32);
 	m->__ipv4_dst = ( (ptr32=platform_packet_get_ipv4_dst(pkt))==NULL ? 0 : *ptr32);
@@ -73,10 +73,10 @@ void fill_packet_matches(datapacket_t *const pkt, packet_matches_t* m){
 	m->__icmpv4_type = ( (ptr8=platform_packet_get_icmpv4_type(pkt))==NULL ? 0 : *ptr8);
 	m->__icmpv4_code = ( (ptr8=platform_packet_get_icmpv4_code(pkt))==NULL ? 0 : *ptr8);
 
-	//MPLS-outermost label 
+	//MPLS-outermost label
 	m->__mpls_label = ( (ptr32=platform_packet_get_mpls_label(pkt))==NULL ? 0 : *ptr32);
 	m->__mpls_tc = ( (ptr8=platform_packet_get_mpls_tc(pkt))==NULL ? 0 : *ptr8);
-	m->__mpls_bos = platform_packet_get_mpls_bos(pkt);		
+	m->__mpls_bos = platform_packet_get_mpls_bos(pkt);
 
 
 	//IPv6
@@ -87,19 +87,19 @@ void fill_packet_matches(datapacket_t *const pkt, packet_matches_t* m){
 	m->__ipv6_nd_sll = ( (ptr64=platform_packet_get_ipv6_nd_sll(pkt))==NULL ? 0 : *ptr64);
 	m->__ipv6_nd_tll = ( (ptr64=platform_packet_get_ipv6_nd_tll(pkt))==NULL ? 0 : *ptr64);
 	m->__ipv6_exthdr = ( (ptr16=platform_packet_get_ipv6_exthdr(pkt))==NULL ? 0 : *ptr16);
-	
-	//ICMPv6 
+
+	//ICMPv6
 	m->__icmpv6_code = ( (ptr8=platform_packet_get_icmpv6_code(pkt))==NULL ? 0 : *ptr8);
 	m->__icmpv6_type = ( (ptr8=platform_packet_get_icmpv6_type(pkt))==NULL ? 0 : *ptr8);
 
 	//PBB
 	m->__pbb_isid = ( (ptr32=platform_packet_get_pbb_isid(pkt))==NULL ? 0 : *ptr32);
-	
+
 	//Tunnel id
 	m->__tunnel_id = ( (ptr64=platform_packet_get_tunnel_id(pkt))==NULL ? 0 : *ptr64);
 
 #ifdef ROFL_EXPERIMENTAL
-	/*	
+	/*
 	* Extensions
 	*/
 
@@ -107,10 +107,10 @@ void fill_packet_matches(datapacket_t *const pkt, packet_matches_t* m){
 	m->__pppoe_code = ( (ptr8=platform_packet_get_pppoe_code(pkt))==NULL ? 0 : *ptr8);
 	m->__pppoe_type = ( (ptr8=platform_packet_get_pppoe_type(pkt))==NULL ? 0 : *ptr8);
 	m->__pppoe_sid = ( (ptr16=platform_packet_get_pppoe_sid(pkt))==NULL ? 0 : *ptr16);
-	
+
 	//PPP related extensions
 	m->__ppp_proto = ( (ptr16=platform_packet_get_ppp_proto(pkt))==NULL ? 0 : *ptr16);
-	
+
 	//GTP related extensions
 	m->__gtp_msg_type = ( (ptr8=platform_packet_get_gtp_msg_type(pkt))==NULL ? 0 : *ptr8);
 	m->__gtp_teid = ( (ptr32=platform_packet_get_gtp_teid(pkt))==NULL ? 0 : *ptr32);
@@ -143,22 +143,22 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 	packet_matches_t matches;
 	packet_matches_t* m = &matches;
 
-	//Prefill packet matches	
+	//Prefill packet matches
 	fill_packet_matches(pkt, m);
-	
-	ROFL_PIPELINE_INFO("Packet[%p] {", pkt);	
-	
+
+	ROFL_PIPELINE_INFO("Packet[%p] {", pkt);
+
 	//Ports
 	if(m->__port_in)
 		ROFL_PIPELINE_INFO_NO_PREFIX("PORT_IN:%u, ",m->__port_in);
 	if(m->__phy_port_in)
 		ROFL_PIPELINE_INFO_NO_PREFIX("PHY_PORT_IN:%u, ",m->__phy_port_in);
-	
+
 	//Metadata
 	if(m->__metadata)
 		ROFL_PIPELINE_INFO_NO_PREFIX("METADATA: 0x%" PRIx64 ", ",m->__metadata);
-	
-	//802	
+
+	//802
 	if(m->__eth_src){
 		uint64_t tmp = m->__eth_src;
 		if(!raw_nbo)
@@ -198,7 +198,7 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 		uint64_t tmp = m->__arp_tha;
 		if(!raw_nbo)
 			tmp = OF1X_MAC_VALUE(NTOHB64(tmp));
-		ROFL_PIPELINE_INFO_NO_PREFIX("ARP_THA:0x%"PRIx64", ", tmp); 
+		ROFL_PIPELINE_INFO_NO_PREFIX("ARP_THA:0x%"PRIx64", ", tmp);
 	}
 	if(m->__eth_type == ETH_TYPE_ARP)
 		ROFL_PIPELINE_INFO_NO_PREFIX("ARP_TPA:0x%x, ", COND_NTOHB32(raw_nbo,m->__arp_tpa));
@@ -208,15 +208,15 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 
 	if((m->__eth_type == ETH_TYPE_IPV4 || m->__eth_type == ETH_TYPE_IPV6) && m->__ip_ecn)
 		ROFL_PIPELINE_INFO_NO_PREFIX("IP_ECN:0x%x, ",m->__ip_ecn);
-	
+
 	if((m->__eth_type == ETH_TYPE_IPV4 || m->__eth_type == ETH_TYPE_IPV6) && m->__ip_dscp){
 		uint8_t tmp = m->__ip_dscp;
 		if(!raw_nbo)
-			tmp = OF1X_IP_DSCP_VALUE(tmp); 
-		
+			tmp = OF1X_IP_DSCP_VALUE(tmp);
+
 		ROFL_PIPELINE_INFO_NO_PREFIX("IP_DSCP:0x%x, ", tmp);
 	}
-	
+
 	if(m->__ipv4_src)
 		ROFL_PIPELINE_INFO_NO_PREFIX("IPV4_SRC:0x%x, ", COND_NTOHB32(raw_nbo,m->__ipv4_src));
 	if(m->__ipv4_dst)
@@ -241,33 +241,33 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 	//ICMPV4
 	if(m->__ip_proto == IP_PROTO_ICMPV4)
 		ROFL_PIPELINE_INFO_NO_PREFIX("ICMPV4_TYPE:%u, ICMPV4_CODE:%u, ",m->__icmpv4_type,m->__icmpv4_code);
-	
+
 	//IPv6
 	if( UINT128__T_LO(m->__ipv6_src) || UINT128__T_HI(m->__ipv6_src) ){
 		uint128__t tmp = m->__ipv6_src;
 		(void)tmp;
 		if(!raw_nbo)
-			NTOHB128(tmp); 	
+			NTOHB128(tmp);
 		ROFL_PIPELINE_INFO_NO_PREFIX("IPV6_SRC:0x%lx:%lx, ",UINT128__T_HI(tmp),UINT128__T_LO(tmp));
 	}
 	if( UINT128__T_LO(m->__ipv6_dst) || UINT128__T_HI(m->__ipv6_dst) ){
 		uint128__t tmp = m->__ipv6_dst;
 		(void)tmp;
 		if(!raw_nbo)
-			NTOHB128(tmp); 
+			NTOHB128(tmp);
 		ROFL_PIPELINE_INFO_NO_PREFIX("IPV6_DST:0x%lx:%lx, ",UINT128__T_HI(tmp),UINT128__T_LO(tmp));
 	}
 	if(m->__eth_type == ETH_TYPE_IPV6){
 		uint32_t tmp = m->__ipv6_flabel;
 		if(!raw_nbo)
-			tmp = OF1X_IP6_FLABEL_VALUE(NTOHB32(tmp));		
+			tmp = OF1X_IP6_FLABEL_VALUE(NTOHB32(tmp));
 		ROFL_PIPELINE_INFO_NO_PREFIX("IPV6_FLABEL:0x%lu, ", tmp);
 	}
 	if(m->__ip_proto == IP_PROTO_ICMPV6){
-		uint128__t tmp = m->__ipv6_nd_target; 
+		uint128__t tmp = m->__ipv6_nd_target;
 		(void)tmp;
 		if(!raw_nbo)
-			NTOHB128(tmp); 	
+			NTOHB128(tmp);
 		ROFL_PIPELINE_INFO_NO_PREFIX("IPV6_ND_TARGET:0x%lx:%lx, ",UINT128__T_HI(tmp),UINT128__T_LO(tmp));
 	}
 	if(m->__ip_proto == IP_PROTO_ICMPV6){
@@ -280,22 +280,22 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 	}
 	if(m->__ip_proto == IP_PROTO_ICMPV6){
 		//NOTE && m->__icmpv6_type ==?
-		uint64_t tmp = m->__ipv6_nd_tll; 
+		uint64_t tmp = m->__ipv6_nd_tll;
 		if(!raw_nbo)
 			tmp = OF1X_MAC_VALUE(NTOHB64(tmp));
 		ROFL_PIPELINE_INFO_NO_PREFIX("IPV6_ND_TLL:0x%"PRIx64", ", tmp);
-	} 
+	}
 	/*TODO IPV6 exthdr*/
 	/*nd_target nd_sll nd_tll exthdr*/
-	
+
 	//ICMPv6
 	if(m->__ip_proto == IP_PROTO_ICMPV6)
 		ROFL_PIPELINE_INFO_NO_PREFIX("ICMPV6_TYPE:%lu, ICMPV6_CODE:%lu, ",m->__icmpv6_type,m->__icmpv6_code);
-	
-	//MPLS	
+
+	//MPLS
    	if(m->__eth_type == ETH_TYPE_MPLS_UNICAST || m->__eth_type == ETH_TYPE_MPLS_MULTICAST ){
 		uint8_t tmp_tc = m->__mpls_tc;
-		uint32_t tmp_label = m->__mpls_label; 
+		uint32_t tmp_label = m->__mpls_label;
 		if(!raw_nbo){
 			tmp_tc = OF1X_MPLS_TC_VALUE(tmp_tc);
 			tmp_label = OF1X_MPLS_LABEL_VALUE(NTOHB32(tmp_label));
@@ -317,7 +317,7 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 		//PPP
 		if(m->__eth_type == ETH_TYPE_PPPOE_SESSION)
 			ROFL_PIPELINE_INFO_NO_PREFIX("PPP_PROTO:0x%x, ",m->__ppp_proto);
-				
+
 	}
 
 	//GTP
@@ -363,9 +363,9 @@ void dump_packet_matches(datapacket_t *const pkt, bool raw_nbo){
 		ROFL_PIPELINE_INFO_NO_PREFIX("GRE_VERSION:0x%x, GRE_PROT_TYPE:0x%x, GRE_KEY:0x%x, ",COND_NTOHB16(raw_nbo, m->__gre_version), COND_NTOHB16(raw_nbo, m->__gre_prot_type), COND_NTOHB32(raw_nbo, m->__gre_key));
 	}
 #endif
-	
-	ROFL_PIPELINE_INFO_NO_PREFIX("}\n");	
 
-	//Add more here...	
+	ROFL_PIPELINE_INFO_NO_PREFIX("}\n");
+
+	//Add more here...
 }
 
