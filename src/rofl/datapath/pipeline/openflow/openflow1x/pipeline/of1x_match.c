c7733675dcf518605d47c3b45774647a25ce39f7
Marc Sune <marc.sune@bisdn.de>
2015-03-09 13:24:45 +0100

    Fixing byte order for PBB tag

--- src/rofl/datapath/pipeline/openflow/openflow1x/pipeline/of1x_match.c
+++ src/rofl/datapath/pipeline/openflow/openflow1x/pipeline/of1x_match.c
@@ -1032,7 +1032,9 @@ of1x_match_t* of1x_init_pbb_isid_match(uint32_t value, uint32_t mask){
 	if(unlikely(match == NULL))
 		return NULL;
 	
-	//TODO Align to pipeline convention (NBO, lower memory address) -- currently not implemented
+	// Align to pipeline convention (NBO, lower memory address)
+	value = HTONB32(value);
+	mask = HTONB32(mask);
 
 	match->type = OF1X_MATCH_PBB_ISID;
 	match->__tern = __init_utern32(value&OF1X_3_BYTE_MASK, mask&OF1X_3_BYTE_MASK); //no wildcard 
